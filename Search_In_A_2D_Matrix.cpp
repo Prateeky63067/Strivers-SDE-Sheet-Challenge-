8f35137d7af7eb841839ee03da0b66e46c4eef7f
Prateek yadav <shubhamy9451@gmail.com>
2023-06-12 17:18:27 +0530

    12/6/2023

---
+++ Search_In_A_2D_Matrix.cpp
@@ -0,0 +1,18 @@
+bool searchMatrix(vector<vector<int>>& mat, int target) {
+    vector<int>store;
+    int n=mat.size();
+    int m=mat[0].size();
+    int i=0,j=m-1;
+    while(i<n&&j>=0)
+    {
+        
+        if(mat[i][j]==target)
+        return true;
+        else if(mat[i][j]>target)
+        j--;
+        else
+        i++;
+    
+    }
+    return false;
+}
