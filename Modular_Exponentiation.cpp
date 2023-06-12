25f6d152664b3770fdf305e9d19a453558fa7d2b
Prateek yadav <shubhamy9451@gmail.com>
2023-06-12 18:11:35 +0530

    12/6/2023

--- Modular_Exponentiation.cpp
+++ Modular_Exponentiation.cpp
@@ -14,3 +14,36 @@ int modularExponentiation(int x, int n, int m) {
  }
  return ans;
 }
+
+// best approch
+#include <bits/stdc++.h>
+
+int findMajorityElement(int arr[], int n) {
+	int cnt=0,el;
+	for(int i=0;i<n;i++)
+	{
+		if(cnt==0)
+		{
+			cnt=1;
+			el=arr[i];
+		}else if(el==arr[i])
+		{
+			cnt++;
+		}else{
+			cnt--;
+		}
+	}
+	int count=0;
+	for(int i=0;i<n;i++)
+	{
+		if(el==arr[i])
+		{
+			count++;
+		}
+
+	}
+	if(count>(n/2))
+	return el;
+	else
+	return -1;
+}
